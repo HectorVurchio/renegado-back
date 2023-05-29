dbc25a5fabdad6c47f7f9b05de3e941ef6567600
Hector Vurchio <hectorvurchio@gmail.com>
2023-05-29 14:31:23 -0400

    first commit

---
+++ src/main/java/com/elrenegado/back/renegadoback/RenegadoBackApplication.java
@@ -0,0 +1,13 @@
+package com.elrenegado.back.renegadoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenegadoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenegadoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elrenegado/back/renegadoback/RenegadoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elrenegado.back.renegadoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenegadoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
